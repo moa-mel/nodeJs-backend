d13a38b2350862fc86b7b31d0a7c9418760b2964
Olaitan <olaitanmaryakano@gmail.com>
2023-12-18 16:03:25 +0100

    initial commit

---
+++ Introduction/Export/app.js
@@ -0,0 +1,4 @@
+const helpers = require('./helpers');
+
+const total = helpers.sum(10, 200);
+console.log("Total:", total)
---
+++ Introduction/objectDestructure/app.js
@@ -0,0 +1,4 @@
+const {sum} = require('./helpers');
+
+const total = sum(10, 200);
+console.log("Total:", total)
