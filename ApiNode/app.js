396d49592b0de40c9e28787507d86b666a72ab46
Olaitan <olaitanmaryakano@gmail.com>
2023-12-21 00:40:09 +0100

    update

--- ApiNode/app.js
+++ ApiNode/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const morgan = require("morgan");
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
 dotenv.config()
 
 //db
@@ -22,6 +23,7 @@ const postRoutes = require('./routes/post')
 //middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json())
+app.use(expressValidator())
 app.use("/", postRoutes)
 
 const port = process.env.PORT || 8080
--- ApiNode/controllers/post.js
+++ ApiNode/controllers/post.js
@@ -1,12 +1,12 @@
 const Post = require("../models/post")
 
 exports.getPosts = (req, res) =>{
-    res.json({
-        posts: [
-            {title: 'First post'},
-            {title: "second post"}
-        ]
+    const posts = Post.find()
+    .select("_id title body")
+    .then(posts=> {
+        res.json({posts})
     })
+    .catch(err=>console.log(err))
 };
 
 exports.createPost = (req, res) => {
@@ -14,13 +14,8 @@ exports.createPost = (req, res) => {
     //console.log("Creating post:", req.body)
     post.save()
     .then(result => {
-        res.status(200).json({
+        res.json({
             post: result
         });
     })
-    .catch(err => {
-        res.status(400).json({
-            error: err
-        });
-    });
 }
